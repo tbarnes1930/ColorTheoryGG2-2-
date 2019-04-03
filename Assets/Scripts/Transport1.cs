a582ba9546d904918a4cf68ca2dafe1e511b85bb
Barrington H. Scott <barrington_scott@bloomfield.edu>
2019-04-03 19:17:33 -0400

    Proper Skyboxes!

---
+++ Assets/Scripts/Transport1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     private void OnTriggerEnter(Collider objectCollided)
+    {
+           GetComponent<Transform>().position = new Vector3(-0.0323f, 0.1037f);
+    }
+}
