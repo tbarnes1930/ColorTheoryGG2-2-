dc714db9e914007b302e7df2a40c3b6358fb627d
AlexanderMatos1997 <47197201+AlexanderMatos1997@users.noreply.github.com>
2019-04-04 16:47:58 -0400

    I Added a Start button in the Title Scene

---
+++ Assets/Scripts/UIScripts/StartButtonFunction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonFunction : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Button Pushed");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
