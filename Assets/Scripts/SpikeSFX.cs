c8f2e8a73ad4e54fc2b69b5841031914eee44971
Kibwe Brandon <47197142+KibweB@users.noreply.github.com>
2019-04-06 13:19:06 -0400

    Red Spike Audio adjustment

--- Assets/Scripts/SpikeSFX.cs
+++ Assets/Scripts/SpikeSFX.cs
@@ -6,16 +6,10 @@ public class SpikeSFX : MonoBehaviour
 {
     public AudioSource SpikeSource;
     
-    //public AudioClip RotateSpike;
-   
-     //[Range(0.0f, 1.0f)]
-    //public float SpikeVolume;
-
-    // Start is called before the first frame update
     void Start()
     {
         SpikeSource = GetComponent<AudioSource>();
-        SpikeSource.PlayDelayed(0.5f);
+        SpikeSource.PlayDelayed(0.8f);
     }
 
 
