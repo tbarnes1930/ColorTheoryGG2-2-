9b9c96c5eb4a3531bbaacbecab7f2c18dd8a38a3
tbarnes1930 <46728262+tbarnes1930@users.noreply.github.com>
2019-04-25 17:02:05 -0400

    Fixed Level Transitions

--- Assets/Scripts/Hit_the_Floor.cs
+++ Assets/Scripts/Hit_the_Floor.cs
@@ -15,17 +15,40 @@ public class Hit_the_Floor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        floorCheck = false;
     }
 
-    private void OnCollisionStay(Collision orange)
+    private void OnCollisionEnter(Collision orange)
     {
         if (orange.gameObject.tag == "Floor")
         {
-            FloorSource.PlayOneShot(SoundOfContact, FallVolume);        
+            floorCheck = true;
+            
+        }
+
+        if (floorCheck == true)
+        {
+            FloorSource.PlayOneShot(SoundOfContact, FallVolume);
             //Debug.Log("The orange has hit the " + orange.gameObject.name);
 
         }
+    }
+
+   /* private void OnCollisionEnter(Collision restart)
+    {
+        if (restart.gameObject.tag == "Respawn")
+        {
+            floorCheck = false;
+        }
+    }*/
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            floorCheck = false;
+        }
+
     }
 
 }
